5751fbc487a18a944a445fe3b342011e83fa2f41
Shimon Tamano <shimogramming@gmail.com>
2017-08-07 12:08:49 +0000

    crud improvements

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,6 +1,7 @@
 class TasksController < ApplicationController
   before_action :set_task, only: [:show, :edit, :update, :destroy]
   before_action :require_user_logged_in, only: [:index, :show]
+  before_action :correct_user, only: [:destroy, :edit]
   
   def index
     # @tasks=Task.all
@@ -67,6 +68,7 @@ class TasksController < ApplicationController
   def task_params
     params.require(:task).permit(:content, :status)
   end
+  
   def correct_user
     @task = current_user.tasks.find_by(id: params[:id])
     unless @task
